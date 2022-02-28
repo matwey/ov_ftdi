b1deb970b38f93ede2b1bd9cf7137a56466b09a0
dogtopus <dogtopus@users.noreply.github.com>
2022-02-28 02:32:31 -0400

    Batch file does not use single quoted strings.

--- software/host/package.bat
+++ software/host/package.bat
@@ -1,4 +1,4 @@
 @echo off
 REM Install PyInstaller into the virtualenv and run this script.
 
-pyinstaller --add-binary 'libov.dll:.' --add-data 'ov3.fwpkg:.' ovctl.py
+pyinstaller --add-binary "libov.dll:." --add-data "ov3.fwpkg:." ovctl.py
